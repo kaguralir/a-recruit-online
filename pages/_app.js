59efcbac2b638b060da9aa923fdab30ab590a865
Nick <princenickballo@gmail.com>
2021-07-20 17:08:41 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../styles/index.scss'
+import '../styles/formulaire.scss'
+import '../styles/recruteur.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
