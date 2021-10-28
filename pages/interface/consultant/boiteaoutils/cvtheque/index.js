2dda05c495fae2b26fe67172bcbea9ae9a9b90ca
kaguralir <arsonistera@gmail.com>
2021-10-28 15:38:53 +0200

    table comptre resultat commencé

--- pages/interface/consultant/boiteaoutils/cvtheque/index.js
+++ pages/interface/consultant/boiteaoutils/cvtheque/index.js
@@ -48,27 +48,6 @@ const allcv = ({ cvs }) => {
                     <div>
                         <h1>Liste de CV</h1>
                     </div>
-                    {/*                     <div className="w100 center orientationV">
-
-                        <div className="search_bar w100">
-                            <form onSubmit={(e) => { e.preventDefault(); }} role="search" className="w100">
-                                <input className="w100" id="search" type="search" placeholder="Rechercher un CV" autoFocus autoComplete="off" required onChange={(e) => { setFilter(e.target.value) }} />
-                            </form>
-                            <ul>
-                                {filteredResult.map((cv) => (
-                                    <li key={cv.candidat_id} className="cv">
-                                        <Link href={{ pathname: cv.origin === "Candidat" ? "/interface/consultant/boiteaoutils/gestionrecrutements/candidat" : "/interface/consultant/boiteaoutils/gestionrecrutements/recruteur", query: { id: cv.id, type: cv.origin } }}>
-                                            <a>
-                                                <div className="url">
-                                                    {cv.user_firstname + " ( " + cv.origin + " ) "}
-                                                </div>
-                                            </a>
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div> */}
 
                     <div className="App">
                         <input type="text" placeholder="Search..." onChange={(event) => { setSearchTerm(event.target.value) }} />
@@ -154,32 +133,6 @@ const allcv = ({ cvs }) => {
                         })}
 
                     </div>
-                    {/*                   <div className="cvCards-container">
-
-                        {cvs.map(cv =>
-
-                            <div id="card">
-                                <div id="content">
-                                    <div id="title">
-                                        Candidat nom: {cv.user_firstname}
-                                    </div>
-                                    <div id="title">
-                                        Candidat prénom: {cv.user_name}
-                                    </div>
-                                    <div id="desc">
-                                        Travail recherché: {cv.searched_job1}
-                                    </div>
-
-
-                                    <div id="info-box">
-
-                                        <button className="btn--pay"><Link href={"/interface/consultant/boiteaoutils/cvtheque/" + cv.candidat_id} className="link-cv">Voir le CV en détail</Link></button>
-                                    </div>
-                                </div>
-                            </div>
-
-                        )}
-                    </div> */}
                 </div>
             </Consultant_layout>
         </div >
--- pages/interface/consultant/boiteaoutils/gestioncomptable/compteresultat.js
+++ pages/interface/consultant/boiteaoutils/gestioncomptable/compteresultat.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from "react"
+/* import React, {useState,useEffect} from "react"
 
 export default function gestionComptable(){
     const [name, setName] = useState();
@@ -65,4 +65,110 @@ export default function gestionComptable(){
             </table>
         </div>
     );
+} */
+
+import React from 'react';
+import Head from 'next/head'
+import Consultant_layout from '../../../../../components/layouts/consultant_layout'
+
+export default function compteresultat({ selectMonth, reset }) {
+
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function getMonthsForLocale(locale) {
+        let format = new Intl.DateTimeFormat(locale, { month: 'long' })
+        let months = []
+        for (let month = 0; month < 12; month++) {
+            let testDate = new Date(Date.UTC(2000, month, 1, 0, 0, 0));
+            months.push(capitalizeFirstLetter(format.format(testDate)))
+        }
+
+        return months;
+    }
+
+
+    const months = getMonthsForLocale('en-EN')
+
+
+    return (
+        <div>
+            <Head>
+                <title>A recruit | CV Thèque</title>
+            </Head>
+
+            <Consultant_layout
+                position="cvtheque"
+            >
+                <table>
+                    <thead>
+
+                        <tr>
+                            <th>2021</th>
+                            {months.map((month) =>
+                                <th>{month}</th>
+                            )}
+
+                        </tr>
+                    </thead>
+                    <tr> <td><a href="#">Bénéfice d’exploitation</a></td>
+                        <td>Paragon</td>
+                        <td>1/5/2021</td>
+                        <td>
+                            <p class="status status-unpaid">Unpaid</p>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td><a href="#">Revenu d’intérêts (charge)</a></td>
+                        <td>Sonic</td>
+                        <td>1/4/2021</td>
+                        <td>
+                            <p class="status status-paid">Paid</p>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td><a href="#">Bénéfice avant impôts</a></td>
+                        <td>Innercircle</td>
+                        <td>1/2/2021</td>
+                        <td>
+                            <p class="status status-pending">Pending</p>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td><a href="#">Charge d’impôts</a></td>
+                        <td>Varsity Plus</td>
+                        <td>12/30/2020</td>
+                        <td>
+                            <p class="status status-pending">Pending</p>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td><a href="#">Revenu net</a></td>
+                        <td>Highlander</td>
+                        <td>12/18/2020</td>
+                        <td>
+                            <p class="status status-paid">Paid</p>
+                        </td>
+
+                    </tr>
+
+                </table>
+            </Consultant_layout>
+        </div>
+
+
+    );
+
+
+
+
+
+
+
 }
